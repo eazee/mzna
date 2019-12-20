80a9b63311a442eb1dcf97c8694fe5dd850992fb
Fleur Budek <fleurbudek@gmail.com>
2019-12-20 10:57:27 +0000

    update syntax and fit string literal lexing

--- Syntax.md
+++ Syntax.md
@@ -1,24 +1,28 @@
 # Syntax
 ```
-program    := filehead (function | assignment)* end
-filehead   := in 
+program    := filehead  colon (function | assignment)* end
+filehead   := "in"  string   "program"   string
 function   := ident  (type)  (params)  block
-params     := param_op   (value)+
+params     := param_op   (value  type  (, value type)+)
 block      := colon  (statement)*  end
 statement  := assignment | call | if | while
+call       := ident  (param_op   value  (,  (value)+))
 if         := "if" expression ifblock
 ifblock    := colon  (statement)*  ("elif" statement*) ("else" statement*) end
 while      := "while" expression  block
 expression := ("(") value  binop  value (")")
 value      := ident | literal | expression
-assignment := value  ass_op  ident
-binop      := ">" | "<" | "<=" | ">=" | "%" | "==" | "+" | "-" | "*" | "/"
-literal    := number | """ (char)* """
+assignment := value  ass_op  ident  (type)
+binop      := ">" | "<" | "<=" | ">=" | "%" | "=?" | "+" | "-" | "*" | "/"
+literal    := number | string | bool
+string     := """ (char)* """
 number     := [0-9]+ | ([0.9])*.([0-9])+
 char       := any character (a, b, c, 0, 1, 2, etc.)
+bool       := "true" | "false"
 ident      := [a-Z]([a-Z | 0-9 | _])*
+type       := "number" | "bool" | "string" | ident
 
-ass_op     := "->"
+ass_op     := "->" | "+->" | "*->" | "/->" | "-->"
 param_op   := "<-"
 colon      := ":"
 end        := "end"
--- src/lex.c
+++ src/lex.c
@@ -19,6 +19,17 @@ char peek(FILE *infile) {
     return c;
 }
 
+int string_not_terminated(char c) {
+    switch(c) {
+        case('"'):
+            return 0;
+        case(EOF):
+            return -1;
+        default:
+            return 1;
+    }
+}
+
 int lex(FILE* infile, token_stream_t* ts) {
     int row = 1, col = 0;
     char c;
@@ -60,7 +71,74 @@ int lex(FILE* infile, token_stream_t* ts) {
             }
         }
 
-        // Integer literals
+        // Parameter operator (<-) OR less than sign (<) OR less than or equal sign (<=)
+        if(c == '<') {
+            if(peek(infile) == '-') {
+                token_stream_add(ts, row, col, "<-", PARAMOP);
+                nextch(infile, &row, &col);
+                continue;
+            } else if(peek(infile) == '=') {
+                token_stream_add(ts, row, col, "<=", LESSTHANOREQ);
+                nextch(infile, &row, &col);
+                continue;
+            } else {
+                token_stream_add(ts, row, col, "<", LESSTHAN);
+                continue;
+            }
+        }
+
+        // Greater than (>) OR greater than or equal (>=)
+        if(c == '>') {
+            if(peek(infile) == '=') {
+                token_stream_add(ts, row, col, ">=", GREATERTHANOREQ);
+                nextch(infile, &row, &col);
+                continue;
+            } else {
+                token_stream_add(ts, row, col, ">", GREATERTHAN);
+                continue;
+            }
+        }
+
+        // Equality comparison operator (=?)
+        if(c == '=') {
+            if(peek(infile) == '?') {
+                token_stream_add(ts, row, col, "=?", COMPOP);
+                nextch(infile, &row, &col);
+                continue;
+            }
+        }
+
+        // Comma (,)
+        if(c == ',') {
+            token_stream_add(ts, row, col, ",", COMMA);
+            continue;
+        }
+
+        // plus (+) OR plus assignment op (+->)
+        if(c == '+') {
+            if(fgetc(infile) == '-' && fgetc(infile) == '>') {
+                token_stream_add(ts, row, col, "+->", ADDASSOP);
+                col += 2;
+                continue;
+            } else {
+                ungetc('>', infile);
+                ungetc('-', infile);
+                token_stream_add(ts, row, col, "+", ADDOP);
+                continue;
+            }
+        }
+        // divide (/)
+        if(c == '/') {
+            token_stream_add(ts, row, col, "/", DIVOP);
+            continue;
+        }
+        // multiply (*)
+        if(c == '*') {
+            token_stream_add(ts, row, col, "*", MULTOP);
+            continue;
+        }
+
+        // Number literals
         if(isdigit(c) || (c == '.' && isdigit(peek(infile)))) {
             size_t strsize = INIT_DIGIT_BUFFER;
             char*  numstr  = malloc(strsize);
@@ -91,8 +169,26 @@ int lex(FILE* infile, token_stream_t* ts) {
             int    i        = 0;
             int    srow     = row;
             int    scol     = col;
+            char   test_c;
 
-            do {
+            while(1) {
+                identstr[i] = c;
+                test_c = peek(infile);
+                
+                i++;
+                if(i > strsize) {
+                    strsize += INCR_IDENT_BUFFER;
+                    identstr = realloc(identstr, strsize);
+                }
+
+                if(isalnum(test_c) || test_c == '_') {
+                    c = nextch(infile, &row, &col);
+                } else {
+                    break;
+                }
+            }
+
+            /*do {
                 identstr[i] = c;
                 c = nextch(infile, &row, &col);
                 i++;
@@ -100,8 +196,10 @@ int lex(FILE* infile, token_stream_t* ts) {
                     strsize += INCR_IDENT_BUFFER;
                     identstr = realloc(identstr, strsize);
                 }
-            } while(isalnum(c) || c == '_');
-            ungetc(c, infile);
+                test_c = peek(infile);
+            } while(isalnum(test_c) || test_c == '_');*/
+            //ungetc(c, infile);
+
             identstr[i] = '\0';
             if(strcmp(identstr, "in") == 0) {
                 token_stream_add(ts, srow, scol, identstr, IN);
@@ -116,30 +214,34 @@ int lex(FILE* infile, token_stream_t* ts) {
 
         // String literals
         if(c == '"') {
-            int   strsize = INIT_STRLIT_BUFFER;
-            char* litstr  = malloc(strsize);
-            int   i       = 0;
-            int   srow    = row;
-            int   scol    = col;
-            c             = nextch(infile, &row, &col);
+            int   term;
+            int   srow = row, scol = col;
+            char  testc  = peek(infile);
+            int   len    = 0;
+            int   size   = INIT_STRLIT_BUFFER;
+            char* strlit = malloc(size);
 
-            while(c != '"') {
-                if(c == EOF) {
-                    fprintf(stderr, "[%sParser Error%s] Unexpected end of file. Expected end of string literal at Row %d, Col %d\n",
-                        TRM_RED_BOLD, TRM_RESET, row, col);
-                    return 1;
-                }
-                litstr[i] = c;
+            while((term = string_not_terminated(testc)) == 1) {
                 c = nextch(infile, &row, &col);
-                i++;
-                if(i > strsize) {
-                    strsize += INCR_STRLIT_BUFFER;
-                    litstr = realloc(litstr, strsize);
+                strlit[len] = c;
+                testc = peek(infile);
+                len++;
+                if(len >= size) {
+                    size += INCR_STRLIT_BUFFER;
+                    strlit = realloc(strlit, size);
                 }
             }
-            litstr[i] = '\0';
-            token_stream_add(ts, srow, scol, litstr, STRING);
-            free(litstr);
+
+            if(term == -1) {
+                fprintf(stderr, "[%sParser Error%s] Unexpected end of file. Expecting termination of string literal at Row %d, Col %d\n",
+                    TRM_RED_BOLD, TRM_RESET, row, col);
+                return 1;
+            }
+
+            strlit[len] = '\0';
+            token_stream_add(ts, srow, scol, strlit, STRING);
+            free(strlit);
+            nextch(infile, &row, &col);
             continue;
         }
 
