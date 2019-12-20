e2a37d638d4e706bebe6279ede45ee7146ca9d0b
Fleur Budek <fleurbudek@gmail.com>
2019-12-20 11:32:10 +0000

    update ast_node_new definition

--- src/ast.c
+++ src/ast.c
@@ -1,6 +1,6 @@
 #include "include/mzna.h"
 
-ast_node_t* ast_node_new(node_type_t type_c, int n_children_c);
-void ast_node_init(ast_node_t* node);
-void ast_node_add_child(ast_node_t* parent_node, ast_node_t* child_node);
-void ast_node_destroy(ast_node_t* node);
+ast_node_t* ast_node_new(node_type_t ntype, node_relation_t nrelation) {}
+void ast_node_init(ast_node_t* node) {}
+void ast_node_add_child(ast_node_t* parent_node, ast_node_t* child_node) {}
+void ast_node_destroy(ast_node_t* node) {}
--- src/include/parse.h
+++ src/include/parse.h
@@ -17,7 +17,7 @@ typedef struct {
 } ast_node_t;
 
 /* ast.c */
-ast_node_t* ast_node_new(node_type_t type_c, int n_children_c);
+ast_node_t* ast_node_new(node_type_t ntype, node_relation_t nrelation);
 void ast_node_init(ast_node_t* node);
 void ast_node_add_child(ast_node_t* parent_node, ast_node_t* child_node);
 void ast_node_destroy(ast_node_t* node);
