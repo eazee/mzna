95de516ece6598c88bc6dad9a7f862fe1e2b5fb6
Fleur Budek <fleurbudek@gmail.com>
2019-12-21 00:14:58 +0000

    start groundwork for AST representation

--- src/ast.c
+++ src/ast.c
@@ -1,6 +1,38 @@
 #include "include/parse.h"
 
-ast_node_t* ast_node_new(node_type_t ntype, node_relation_t nrelation) {}
-void ast_node_init(ast_node_t* node) {}
-void ast_node_add_child(ast_node_t* parent_node, ast_node_t* child_node) {}
-void ast_node_destroy(ast_node_t* node) {}
+void ast_node_init(ast_node_t* node, node_type_t ntype, node_relation_t nrelation) {
+    node->type = ntype;
+    node->relation = nrelation;
+    node->n_children = 0;
+    node->value_type = NONE;
+    node->buffer_size = INIT_NODE_BUFFER;
+    node->children = (ast_node_t**) malloc(node->buffer_size * sizeof(ast_node_t*));
+}
+void ast_node_assign_integer(ast_node_t* node, int nvalue) {
+    node->value.integer = nvalue;
+    node->value_type = INTEGER;
+}
+void ast_node_assign_string(ast_node_t* node, char* nvalue) {
+    node->value.string = (char*) malloc(strlen(nvalue)+1);
+    strcpy(node->value.string, nvalue);
+    node->value_type = STRING;
+}
+void ast_node_add_child(ast_node_t* parent_node, ast_node_t* child_node) {
+    if(parent_node->n_children >= parent_node->buffer_size) {
+        parent_node->buffer_size += INCR_NODE_BUFFER;
+        parent_node->children = (ast_node_t**) realloc(parent_node->buffer_size * sizeof(ast_node_t*));
+    }
+    parent_node->children[parent_node->n_children] = child_node;
+    parent_node->n_children++;
+}
+void ast_node_destroy(ast_node_t* node) {
+    if(node->n_children > 0) {
+        for(int i = 0; i < node->n_children; i++) {
+            ast_node_destroy(node->children[i]);
+        }
+    }
+    if(node->value_type == STRING) {
+        free(node->value.string);
+    }
+    free(node->children);
+}
--- src/include/parse.h
+++ src/include/parse.h
@@ -1,26 +1,32 @@
 #ifndef _parse_h
 #define _parse_h
 
+#define INIT_NODE_BUFFER 3;
+#define INCR_NODE_BUFFER 2;
+
 #include <stddef.h>     // size_t
 
 typedef enum { ROOT, ASSIGN, LITERAL, VARIABLE, BINOP, COMPOP, FUNCTION, CALL, BLOCK } node_type_t;
 typedef enum { CONDITION, BODY, IFBODY, ELSEBODY, PARAMETER, SOURCE, DESTINATION } node_relation_t;
+typedef enum { STRING, INTEGER, NONE } node_value_type_t;
 typedef union {
     char* string;
     int   integer;
-} ast_value_t;
+} node_value_t;
 typedef struct {
-    node_type_t     type;
-    node_relation_t relation;
-    ast_value_t     value;
-    size_t           n_children;
-    size_t           buffer_size;
-    ast_node_t**    children;
+    node_type_t       type;
+    node_relation_t   relation;
+    node_value_t      value;
+    node_value_type_t value_type;
+    size_t            n_children;
+    size_t            buffer_size;
+    ast_node_t**      children;
 } ast_node_t;
 
 /* ast.c */
-ast_node_t* ast_node_new(node_type_t ntype, node_relation_t nrelation);
-void ast_node_init(ast_node_t* node);
+void ast_node_init(ast_node_t* node, node_type_t ntype, node_relation_t nrelation);
+void ast_node_assign_integer(ast_node_t* node, int nvalue);
+void ast_node_assign_string(ast_node_t* node, char* nvalue);
 void ast_node_add_child(ast_node_t* parent_node, ast_node_t* child_node);
 void ast_node_destroy(ast_node_t* node);
 
